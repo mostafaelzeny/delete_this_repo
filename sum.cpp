c5219ed2142592924c42e61f8b7bfe93011be26a
mostafaelzeny <elzenymostafa8@gmail.com>
2026-01-01 21:52:07 +0200

    first commit

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1 = 5;
+    int num2 = 7;
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum(num1, num2) << std::endl;
+    return 0;
+}
