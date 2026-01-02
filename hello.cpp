9ef7e54d79619231bf8f11316f0f4dd4f4b709a3
mostafaelzeny <114350893+mostafaelzeny@users.noreply.github.com>
2026-01-02 14:31:28 +0200

    Add output for 'edits_on_remote' in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout << "hello world" << std::endl;
     std::cout << "test1" << std::endl;
     std::cout << "test2" << std::endl;
+    std::cout << "edits_on_remote" << std:: endl;
 
     return 0;
 }
